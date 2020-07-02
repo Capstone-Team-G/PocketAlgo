92773e59a5a5d74f7f87a8f1382f4f1814b76e88
Alex <aleksei_pavlov@yahoo.com>
2020-07-02 15:01:16 -0400

    boiler plate

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  watch: false, 
-  mode: 'development', 
-  entry: './components/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: __dirname+ 'public'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
+    mode: 'development',
+    entry: ['babel-polyfill', './components/index.js'],
+    // entry: './components/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
       },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
         ]
-      }
-    ]
-  },
+    },
 };
