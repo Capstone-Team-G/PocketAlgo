1d26e075f7a361f350b521484d8130bd94b63ba1
Alex <aleksei_pavlov@yahoo.com>
2020-07-06 12:31:53 -0400

    aws works

--- components/index.js
+++ components/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import App from './App';
+import config from '../src/aws-exports'
+Amplify.configure(config)
 
 ReactDOM.render(
-  <h1>Hello, ALGO!!</h1>,
+  <div>
+    <h1>Hello, ALGO!!</h1>
+    <App />
+  </div>,
   document.getElementById('root')
 );
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,174 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateCategories = /* GraphQL */ `
+  subscription OnCreateCategories {
+    onCreateCategories {
+      id
+      name
+      algorithms {
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateCategories = /* GraphQL */ `
+  subscription OnUpdateCategories {
+    onUpdateCategories {
+      id
+      name
+      algorithms {
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteCategories = /* GraphQL */ `
+  subscription OnDeleteCategories {
+    onDeleteCategories {
+      id
+      name
+      algorithms {
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateAlgorithms = /* GraphQL */ `
+  subscription OnCreateAlgorithms {
+    onCreateAlgorithms {
+      id
+      name
+      categoriesID
+      example
+      description
+      categories {
+        id
+        name
+        createdAt
+        updatedAt
+      }
+      quizes {
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateAlgorithms = /* GraphQL */ `
+  subscription OnUpdateAlgorithms {
+    onUpdateAlgorithms {
+      id
+      name
+      categoriesID
+      example
+      description
+      categories {
+        id
+        name
+        createdAt
+        updatedAt
+      }
+      quizes {
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteAlgorithms = /* GraphQL */ `
+  subscription OnDeleteAlgorithms {
+    onDeleteAlgorithms {
+      id
+      name
+      categoriesID
+      example
+      description
+      categories {
+        id
+        name
+        createdAt
+        updatedAt
+      }
+      quizes {
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateQuizes = /* GraphQL */ `
+  subscription OnCreateQuizes {
+    onCreateQuizes {
+      id
+      algorithmsID
+      algorithms {
+        id
+        name
+        categoriesID
+        example
+        description
+        createdAt
+        updatedAt
+      }
+      question
+      option
+      answer
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateQuizes = /* GraphQL */ `
+  subscription OnUpdateQuizes {
+    onUpdateQuizes {
+      id
+      algorithmsID
+      algorithms {
+        id
+        name
+        categoriesID
+        example
+        description
+        createdAt
+        updatedAt
+      }
+      question
+      option
+      answer
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteQuizes = /* GraphQL */ `
+  subscription OnDeleteQuizes {
+    onDeleteQuizes {
+      id
+      algorithmsID
+      algorithms {
+        id
+        name
+        categoriesID
+        example
+        description
+        createdAt
+        updatedAt
+      }
+      question
+      option
+      answer
+      createdAt
+      updatedAt
+    }
+  }
+`;
