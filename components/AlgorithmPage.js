a924519ad0adbe3a73b1a359fbf753bd8701da8a
Alex <aleksei_pavlov@yahoo.com>
2020-07-07 16:14:10 -0400

    added plot

--- components/AlgorithmPage.js
+++ components/AlgorithmPage.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { API, graphqlOperation } from 'aws-amplify'
-import { listAlgorithmss,getSubcategories,listSubcategoriess} from '../src/graphql/queries'
+import { listAlgorithmss, getSubcategories, listSubcategoriess } from '../src/graphql/queries'
 import AlgoViewFunction from './AlgoViewFunction';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 export default function AlgorithmPage() {
     const [subCategory, setSubCategory] = useState([]);
@@ -12,15 +13,7 @@ export default function AlgorithmPage() {
         const id = 'ecfc3985-c704-4511-9474-03f6ff7404d3'//dummy data (bubble sort)
         async function fetchAPI() {
             try {
-            //     const apiData = await API.graphql(graphqlOperation(listSubcategoriess, {
-            //         filter: {
-            //             id: {
-            //                 eq: id
-            //             }
-            //         }
-            //     }
-            //     ))
-                const apiData = await API.graphql(graphqlOperation(getSubcategories,{id:id}));
+                const apiData = await API.graphql(graphqlOperation(getSubcategories, { id: id }));
                 setAlgorithms(apiData.data.getSubcategories.algorithms);
                 setSubCategory(apiData.data.getSubcategories);
             } catch (err) {
@@ -28,11 +21,27 @@ export default function AlgorithmPage() {
             }
         }
         fetchAPI();
-        
+
     }, [])
 
-    console.log(subCategory,algorithms);
+    const data = [{ name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+    { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+    { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+    { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+    { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+    { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+    { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },];
+    console.log(subCategory, algorithms);
     return (
-        <AlgoViewFunction algo={subCategory} />
+        <div>
+            <AlgoViewFunction algo={subCategory} />
+            <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+            </LineChart>
+        </div>
     )
 }
