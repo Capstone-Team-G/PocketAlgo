425736ee3e872429dd2940254c56db593143015a
Alex <aleksei_pavlov@yahoo.com>
2020-07-09 18:49:24 -0400

    step back

--- components/Routes.js
+++ components/Routes.js
@@ -10,10 +10,10 @@ class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="https://master.d1utv16exoeqqk.amplifyapp.com/" component={Home} />
-        <Route exact path="https://master.d1utv16exoeqqk.amplifyapp.com/bycategories" component={AlgosByCategories} />
-        <Route exact path="https://master.d1utv16exoeqqk.amplifyapp.com/algorithms/:id" component={AlgorithmPage} />
-        <Route exact path="https://master.d1utv16exoeqqk.amplifyapp.com/datastructures/:id" component={DataStructurePage} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/bycategories" component={AlgosByCategories} />
+        <Route exact path="/algorithms/:id" component={AlgorithmPage} />
+        <Route exact path="/datastructures/:id" component={DataStructurePage} />
       </Switch>
     )
   }
