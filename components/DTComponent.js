469dfc2028dc6697feec5d25c23258948d4edf71
Dzirt2006 <dzirt-2006@mail.ru>
2020-07-14 17:41:11 -0400

    added algo of the day randomizer + regular  randomizer, fixed state

--- components/DTComponent.js
+++ components/DTComponent.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { API, graphqlOperation } from 'aws-amplify'
 import { getSubcategories } from '../src/graphql/queries'
 import AlgoViewFunction from './AlgoViewFunction'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Header from './material-ui/headertemplate'
+import Footer from './material-ui/footertemplate'
+export default function
 
-export default function DataStructurePage(props) {
+DataStructurePage(props) {
     const [algorithms, setAlgorithms] = useState([]);
     const [dataStructure, setDataStructure] = useState([]);
 
     useEffect(() => {
         const id = props.match.params.id //takes id from url
-        // const id = '30b07844-53a4-4340-ae25-a4e41d4a12ea'//dummy data 
+        // const id = '30b07844-53a4-4340-ae25-a4e41d4a12ea'//dummy data
         async function fetchAPI() {
             try {
                 const apiData = await API.graphql(graphqlOperation(getSubcategories, { id: id }));
@@ -24,13 +29,46 @@ export default function DataStructurePage(props) {
 
     return (
         <div>
+            <Header />
+            <Grid container spacing={3}>
+
+            <Grid item xs={12}>
+            <Paper>
             <AlgoViewFunction data={dataStructure} />
+            </Paper>
+            </Grid>
+                {/* Image */}
+            <Grid item xs={12}>
+            <Paper>
+            {/* Place Component Below This Line */}
             {dataStructure.picture !== null &&
                 <img src={dataStructure.picture} />}
-            <h5>Algorithms related to {dataStructure.name}:</h5>
+            </Paper>
+            </Grid>
+
+            {/* Algo Example */}
+            <Grid item xs={12}>
+            <Paper>
+            {/* Place Component Below This Line */}
+            <h5 align='center'>Algorithms related to {dataStructure.name}:</h5>
+            </Paper>
+            </Grid>
+
+            <Grid item xs={12}>
+            <Paper>
             {algorithms.map(algorithm => (
-                <p onClick={() => props.history.push(`/algorithms/${algorithm.id}`)}>{algorithm.name}</p>
+
+            <p onClick={() => props.history.push(`/algorithms/${algorithm.id}`)} align='center'>{algorithm.name}</p>
+
             ))}
+             </Paper>
+            </Grid>
+            </Grid>
+            <Footer />
         </div>
     )
 }
+
+
+
+
