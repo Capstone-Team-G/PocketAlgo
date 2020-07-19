91bc7db5b1e927e7d34ecc9b959a09238f9034c1
Cesar Perez <cpj8826@gmail.com>
2020-07-19 16:27:03 -0400

    new algo buttons

--- components/Animation.js
+++ components/Animation.js
@@ -30,23 +30,19 @@ export default function Animation(props) {
       }} variant="outlined" color="inherit" style={{ color: 'green' ,  fontSize: 20 , textDecoration: 'none', paddingbottom: 10}}>
                    PREVIOUS
                   </Button> */}
-
-      <button id='previous' type='button' onClick={() => {
+        <Button paddingbottom="20px" paddingRight="20px" variant="outlined" color="inherit" style={{ color: 'green' ,  fontSize: 12 , fontWeight: 'bold', textDecoration: 'none'}}>
+      <p id='previous' onClick={() => {
         prevButtonCheck()
         setPointer(pointer - 1);
-      }}>PREVIOUS</button>
+      }}>PREV.</p>
+        </Button>
 
-      {/* <Button id='previous' onClick={() => {
+        <Button paddingbottom="20px" paddingLeft="20px" variant="outlined" color="inherit" style={{ color: 'green' ,  fontSize: 12 , fontWeight: 'bold', textDecoration: 'none'}}>
+      <p id='next' onClick={() => {
         nextButtonCheck()
         setPointer(pointer + 1);
-      }} variant="outlined" color="inherit" style={{ color: 'green' ,  fontSize: 20 , textDecoration: 'none' , paddingbottom: 10}}>
-                   NEXT
-                  </Button> */}
-
-      <button id='next' type='button' onClick={() => {
-        nextButtonCheck()
-        setPointer(pointer + 1);
-      }}>NEXT</button>
+      }}>NEXT</p>
+      </Button>
       <br></br>
       <div align='left' padding='20px'><h1 style= {{color: 'black', fontFamily: 'true north' , fontFamily: 'Rock Salt', fontSize: 14}} align= "center"> <span style={{color: 'gold' , fontSize: 12}}> /* </span> P o w e r _ Y o u r _ C o d e : <span style={{color: 'gold' , fontSize: 12}}> */ </span> <img src='https://media.istockphoto.com/vectors/cartoon-of-businessman-holding-arrow-sign-pointing-down-vector-id926829172?k=6&m=926829172&s=612x612&w=0&h=DZFHTii7JE8zqJJMNWbcV1nhHaaOVvmcQ4JpoFS1dLA=' height='80px' width='80px' ></img></h1></div>
 
